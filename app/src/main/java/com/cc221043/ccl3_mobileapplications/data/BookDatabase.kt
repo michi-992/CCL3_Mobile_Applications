21fb32eb3ae66a5b2c4021297170290073c8d8a9
michi-992 <152781506+michi-992@users.noreply.github.com>
2024-01-17 16:01:14 +0100

    Added TypeConverters

--- app/src/main/java/com/cc221043/ccl3_mobileapplications/data/BookDatabase.kt
+++ app/src/main/java/com/cc221043/ccl3_mobileapplications/data/BookDatabase.kt
@@ -2,9 +2,11 @@ package com.cc221043.ccl3_mobileapplications.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.cc221043.ccl3_mobileapplications.data.model.Book
 
 @Database(entities = [Book::class], version = 1)
+@TypeConverters(com.cc221043.ccl3_mobileapplications.data.TypeConverters::class)
 abstract class BookDatabase : RoomDatabase() {
     abstract val dao: BookDao
 }
---
+++ app/src/main/java/com/cc221043/ccl3_mobileapplications/data/TypeConverters.kt
@@ -0,0 +1,15 @@
+package com.cc221043.ccl3_mobileapplications.data
+
+import androidx.room.TypeConverter
+
+class TypeConverters {
+    @TypeConverter
+    fun fromList(list: List<String>?): String? {
+        return list?.joinToString(separator = "•") { it }
+    }
+
+    @TypeConverter
+    fun toList(value: String?): List<String>? {
+        return value?.split("•")?.map { it.trim() }
+    }
+}

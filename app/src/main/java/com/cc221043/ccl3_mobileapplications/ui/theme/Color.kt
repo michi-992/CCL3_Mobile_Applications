ca024288692cc660219318991ccd1405f6198623
michi-992 <152781506+michi-992@users.noreply.github.com>
2024-01-18 11:47:23 +0100

    Input custom colours

--- app/src/main/java/com/cc221043/ccl3_mobileapplications/ui/theme/Color.kt
+++ app/src/main/java/com/cc221043/ccl3_mobileapplications/ui/theme/Color.kt
@@ -2,10 +2,25 @@ package com.cc221043.ccl3_mobileapplications.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+object Colors {
+    val Blue0 = Color(0xFF101322)
+    val Blue1 = Color(0xFF181E35)
+    val Blue2 = Color(0xFF1B2138)
+    val Blue3 = Color(0xFF1F253C)
+    val Blue4 = Color(0xFF47537E)
+    val Blue5 = Color(0xFF838CB0)
+    val Blue6 = Color(0xFFCBD6FF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+    val OffWhite = Color(0xFFE8F4F0)
+
+    val PrimaryBlue = Color(0xFF0393D1)
+    val PrimaryBlueDark = Color(0xFF006692)
+
+    val LightGreen = Color(0xFFD3E1A0)
+    val MediumGreen = Color(0xFFA1C698)
+    val DarkGreen = Color(0xFF5B8464)
+
+    val LightRed = Color(0xFFF49595)
+    val MediumRed = Color(0xFFD26161)
+    val DarkRed = Color(0xFFB83636)
+}

dbee4772e0a081d90577f31a45b360554867d361
michi-992 <152781506+michi-992@users.noreply.github.com>
2024-01-19 16:52:43 +0100

    Added db, mvm, imagepick to MainActivity

--- app/src/main/java/com/cc221043/ccl3_mobileapplications/MainActivity.kt
+++ app/src/main/java/com/cc221043/ccl3_mobileapplications/MainActivity.kt
@@ -3,20 +3,57 @@ package com.cc221043.ccl3_mobileapplications
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.room.Room
+import com.cc221043.ccl3_mobileapplications.data.BookDatabase
 import com.cc221043.ccl3_mobileapplications.ui.theme.CCL3MobileApplicationsTheme
+import com.cc221043.ccl3_mobileapplications.ui.view.BoomBitch
+import com.cc221043.ccl3_mobileapplications.ui.view.MainView
+import com.cc221043.ccl3_mobileapplications.ui.view_model.MainViewModel
 
 class MainActivity : ComponentActivity() {
+    private val db by lazy {
+    Room.databaseBuilder(this, BookDatabase::class.java, "BookDatabase.db").build()
+}
+
+    private val mainViewModel by viewModels<MainViewModel> (
+        factoryProducer = {
+            object: ViewModelProvider.Factory {
+                override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                    return MainViewModel(db.dao, this@MainActivity) as T
+                }
+            }
+        }
+    )
+
+    private val pickImageLauncher =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+            // Handle the returned image URI here
+            uri?.let {
+                mainViewModel.updateImageURI(it)
+                println(mainViewModel.mainViewState.value.selectedImageURI)
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         // Handle the splash screen transition.
-        installSplashScreen()
+//        installSplashScreen()
 
         super.onCreate(savedInstanceState)
         setContent {
@@ -26,7 +63,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Text(text = "First tests")
+                    MainView(mainViewModel, pickImageLauncher)
                 }
             }
         }
